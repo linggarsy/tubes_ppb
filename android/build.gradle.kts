01263a90e3b2c7d7fb5fb9d9d894825b5b438bd5
Linggar <linggarsulisyanti@gmail.com>
2026-05-30 16:02:35 +0700

    update code

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -1,3 +1,15 @@
+buildscript {
+    repositories {
+        google()
+        mavenCentral()
+    }
+    dependencies {
+        classpath("com.android.tools.build:gradle:7.3.0")
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.9.0")
+        classpath("com.google.gms:google-services:4.4.0")
+    }
+}
+
 allprojects {
     repositories {
         google()
@@ -21,8 +33,4 @@ subprojects {
 
 tasks.register<Delete>("clean") {
     delete(rootProject.layout.buildDirectory)
-}
-
-dependencies {
-    classpath 'com.google.gms:google-services:4.4.0'
 }
